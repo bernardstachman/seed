6d38ec533bff0bfac7235de6df4931b70cdc23ee
Dan Ward <danward@gmail.com>
2015-04-02 16:40:10 -0400

    Improved directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -105,8 +105,8 @@ gulp.task('watch', function() {
 gulp.task('connect', function() {
 	connect.server({
 		root: outputDir,
-		host: '0.0.0.0',
-// 		host: 'localhost',
+		// host: '0.0.0.0',
+		host: 'localhost',
 		livereload: {
 			enabled: true,
 			port: '4002'			
