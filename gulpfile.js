d77eb681a096a57b1cfc4e2ca2684747358be3ad
Daniel Ward <dan@Daniels-MacBook-Pro.local>
2015-05-15 11:37:15 -0400

    Updated gulpfile flow

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ if (env === 'dev') { //If env = def, we are in dev mode
 // [Define sources]
 jsSrc = ['components/scripts/*.js', 'components/scripts/**/*.js'];
 dataSrc = ['components/data/*.json'];
-indexSrc = ['components/*.html'];
+htmlSrc = ['components/*.html'];
 viewsSrc = ['components/views/*.html'];
 lessSrc = ['components/less/bootstrap.less'];
 cssSrc = ['components/css/*.css'];
@@ -45,10 +45,10 @@ imgSrc = ['components/img/*.*'];
 
 //Process HTML
 gulp.task('html', function() {
-	gulp.src(indexSrc)
+	gulp.src(htmlSrc)
 		.pipe(gulpif(env === 'production', minifyHTML()))  //If in production, minify...
 		.pipe(gulp.dest(outputDir))  //Place in output dir
-		.pipe(connect.reload())  //Reload page to reflect changes
+		.pipe(connect.reload());  //Reload page to reflect changes
 });
 
 //Process views
@@ -56,7 +56,7 @@ gulp.task('views', function() {
 	gulp.src(viewsSrc)
 //		.pipe(gulpif(env === 'production', minifyHTML()))  //If in production, minify...
 		.pipe(gulp.dest(outputDir + '/views'))  //Place in production folder
-		.pipe(connect.reload())  //Reload page to reflect changes
+		.pipe(connect.reload());  //Reload page to reflect changes
 });
 
 //Process JSON data
@@ -64,7 +64,7 @@ gulp.task('json', function() {
 	gulp.src(dataSrc)
 		.pipe(gulpif(env === 'production', minifyJSON()))  //If in production, minify
 		.pipe(gulp.dest(outputDir + '/js/data'))  //Place in output dir
-		.pipe(connect.reload())
+		.pipe(connect.reload());
 });
 
 //Process JavaScript
@@ -74,7 +74,7 @@ gulp.task('js', function() {
 		.pipe(browserify())  //Inject required technologies
 		.pipe(gulpif(env === 'production', uglify()))  //Minify JavaScript
 		.pipe(gulp.dest(outputDir + '/js'))  //Place in ouput dir
-		.pipe(connect.reload())  //Reload page to reflect changes
+		.pipe(connect.reload());  //Reload page to reflect changes
 });
  
 gulp.task('less', function () {
@@ -90,13 +90,13 @@ gulp.task('css', function() {
 	gulp.src(cssSrc)
 		.pipe(gulpif(env === 'production', minifyCSS()))  //If in production, minify...		
 		.pipe(gulp.dest(outputDir + '/css'))  //Place in output dir
-		.pipe(connect.reload())  //Reload page to reflect changes
+		.pipe(connect.reload());  //Reload page to reflect changes
 });
 
 gulp.task('img', function() {
 	gulp.src(imgSrc)
 		.pipe(gulp.dest(outputDir + '/img'))  //Place in output dir
-		.pipe(connect.reload())  //Reload page to reflect changes
+		.pipe(connect.reload());  //Reload page to reflect changes
 });
 
 //Watch for any changes, update when dedected
@@ -105,20 +105,21 @@ gulp.task('watch', function() {
 	gulp.watch(htmlSrc, ['html']);
 	gulp.watch(dataSrc, ['json']);
 	gulp.watch(viewsSrc, ['views']);
-	gulp.watch(lessSrc, ['less']);
+	gulp.watch(lessSrc, ['less']);	
 	gulp.watch(cssSrc, ['css']);
-	gulp.watch(cssSrc, ['img']);	
+	gulp.watch(imgSrc, ['img']);	
 });
 
 //Connect task for livereload server
 gulp.task('connect', function() {
 	connect.server({
+		port: '3000',
 		root: outputDir,
-		host: '0.0.0.0',
-// 		host: 'localhost',
+// 		host: '0.0.0.0',
+		host: 'localhost',
 		livereload: {
-			enabled: true,
-			port: '4002'			
+// 			enabled: true,
+			port: '80'			
 		}
 	});
 });
